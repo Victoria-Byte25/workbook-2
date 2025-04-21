a09418d9438d00a4e4005c2de23d296710208d00
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-20 23:26:13 -0500

    Completed looping exercises: while, do/while, and for loop

---
+++ workbook-2/LoopingExercisesApp/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class WhileLoop {
+        public static void main(String[] args) {
+            int count = 0; // start counting from 0
+            while (count < 5) {
+                System.out.println("I love Java");
+                count++; // increase the count by 1
+            }
+        }
+
+}
