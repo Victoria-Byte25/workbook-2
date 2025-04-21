2e703741a46b76daf817be87f51953b87df882e3
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-20 22:52:52 -0500

    Added 5 input parameters

--- workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -1,66 +1,68 @@
 package com.pluralsight;
 
 public class CellPhone {
-    // Instance variables (data members)
-    private int serialNumber;
+    private String brand;
     private String model;
-    private String carrier;
     private String phoneNumber;
-    private String owner;
+    private String color;
+    private int storage;
 
-    // Parameterless constructor (sets default values)
-    public CellPhone() {
-        this.serialNumber = 0;
-        this.model = "";
-        this.carrier = "";
-        this.phoneNumber = "";
-        this.owner = "";
-    }
+    // Default constructor
+    public CellPhone() {}
 
-    // Getters
-    public int getSerialNumber() {
-        return serialNumber;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getCarrier() {
-        return carrier;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getOwner() {
-        return owner;
+    // Overloaded constructor with 5 parameters
+    public CellPhone(String brand, String model, String phoneNumber, String color, int storage) {
+        this.brand = brand;
+        this.model = model;
+        this.phoneNumber = phoneNumber;
+        this.color = color;
+        this.storage = storage;
     }
 
     // Setters
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public void setModel(String model) {
         this.model = model;
     }
 
-    public void setCarrier(String carrier) {
-        this.carrier = carrier;
-    }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-    // New dial() method
-    public void dial(String phoneToCall) {
-        System.out.println(owner + " 's phone is calling" + phoneToCall);
+    public void setColor(String color) {
+        this.color = color;
     }
 
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    // Getters (for use in dial(CellPhone))
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Display method
+    public void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Color: " + color);
+        System.out.println("Storage: " + storage + "GB");
+    }
+
+    // Original dial method
+    public void dial(String phoneNumber) {
+        System.out.println("Dialing " + phoneNumber + "...");
+    }
+
+    // Overloaded dial method
+    public void dial(CellPhone phone) {
+        System.out.println("Dialing " + phone.getPhoneNumber() + " from " + this.phoneNumber + "...");
+    }
 }
+
+
--- workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -4,59 +4,32 @@ import java.util.Scanner;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Create a phone using overloaded constructor
+        CellPhone phone1 = new CellPhone("Apple", "iPhone 14", "555-1234", "Blue", 128);
 
-        // Step 2: Create the CellPhone object
-        CellPhone cellPhone1 = new CellPhone();
+        // Create a second phone using default constructor and setters
+        CellPhone phone2 = new CellPhone();
+        phone2.setBrand("Samsung");
+        phone2.setModel("Galaxy S23");
+        phone2.setPhoneNumber("555-5678");
+        phone2.setColor("Black");
+        phone2.setStorage(256);
 
-        // Prompt user and set values using setters
-        System.out.print("Enter info for phone 1\n");
-        System.out.print("Serial Number: ");
-        cellPhone1.setSerialNumber(Integer.parseInt(scanner.nextLine()));
+        // Display both phones
+        System.out.println("Phone 1 info:");
+        phone1.display();
+        System.out.println("\nPhone 2 info:");
+        phone2.display();
 
-        System.out.print("Model: ");
-        cellPhone1.setModel(scanner.nextLine());
+        // Dial phone2 from phone1 (String version)
+        System.out.println("\nDial using phone number:");
+        phone1.dial(phone2.getPhoneNumber());
 
-        System.out.print("Carrier: ");
-        cellPhone1.setCarrier(scanner.nextLine());
-
-        System.out.print("Phone number: ");
-        cellPhone1.setPhoneNumber(scanner.nextLine());
-
-        System.out.print("Owner: ");
-        cellPhone1.setOwner(scanner.nextLine());
-
-        // Second phone
-        CellPhone cellPhone2 = new CellPhone();
-        System.out.print("\nEnter info for phone 2\n");
-        System.out.print("Serial Number:");
-        cellPhone2.setSerialNumber(Integer.parseInt(scanner.nextLine()));
-        System.out.print("Model");
-        cellPhone2.setModel(scanner.nextLine());
-        System.out.print("Carrier: ");
-        cellPhone2.setCarrier(scanner.nextLine());
-        System.out.print("Phone Number: ");
-        cellPhone2.setPhoneNumber(scanner.nextLine());
-        System.out.print("Owner: ");
-        cellPhone2.setOwner(scanner.nextLine());
-
-        //Display method
-        System.out.println("\n=== phone Calls ===");
-        cellPhone1.dial(cellPhone2.getPhoneNumber());
-        cellPhone2.dial(cellPhone1.getPhoneNumber());
-
-        scanner.close();
+        // Dial phone2 from phone1 (Object version)
+        System.out.println("\nDial using CellPhone object:");
+        phone1.dial(phone2);
     }
-
-        // Static display
-        public static void display(CellPhone phone) {
-            System.out.println("Owner: " + phone.getOwner());
-            System.out.println("Model: " + phone.getModel());
-            System.out.println("Carrier: " + phone.getCarrier());
-            System.out.println("Phone Number: " + phone.getPhoneNumber());
-            System.out.println("Serial Number: " + phone.getSerialNumber());
-            System.out.println("--------------------------");
-
-        }
-
 }
+
+
+
